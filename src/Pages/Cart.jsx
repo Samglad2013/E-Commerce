8fcb8875e08c355827a77d4e9de4e3099010312e
GladysOwhotemu <gladysowhotemu@gmail.com>
2024-01-25 13:36:41 +0100

    responsiveness

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import CartItems from "../Component/CartItems/CartItems";
 
 const Cart = () => {
-  return (
-    <div>
-      <CartItems />
-    </div>
+  const { cartItems } = useContext(ShopContext);
+  const isCartEmpty = Object.values(cartItems).every(
+    (quantity) => quantity === 0
   );
+
+  return <div>{isCartEmpty ? <p>Your cart is empty</p> : <CartItems />}</div>;
 };
 
 export default Cart;
